02887b7e1066c436f1dce72e5c5646e1dcabd3da
Jose IV <ivjosesantos@gmail.com>
2024-01-27 14:05:55 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', 'tsconfig.node.json'],
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+
+  globals: {
+    React: 'readonly',
+  },
+  ignorePatterns: ['dist/'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-cycle': 'off',
+    'react/require-default-props': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+  },
+}
---
+++ src/Button.tsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <div>test</div>
+}
+
+export default Button
