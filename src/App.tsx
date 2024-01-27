7fdc4889ec41319e3886293e8e9da5c7d8804e5f
Jose IV <ivjosesantos@gmail.com>
2024-01-27 14:42:04 +0800

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,6 @@
-import { useState } from 'react'
-import viteLogo from '../public/vite.svg'
-
-import './App.css'
-import Button from './Button'
-import reactLogo from './assets/react.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount((prevCount) => prevCount + 1)}
-        >
-          count is {count}
-        </button>
-        <Button />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1 className="text-3xl font-bold underline text-red-800">Hello world!</h1>
   )
 }
 
--- src/Button.tsx
+++
@@ -1,5 +0,0 @@
-function Button() {
-  return <div>test</div>
-}
-
-export default Button
